ba06d6f6e00a39bc424d319658b3ba108cbc76b1
sakon-ziwave <ryo.development0320@gmail.com>
2023-05-08 16:36:27 +0900

    functions

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet
+from . import views
 
 router = DefaultRouter()
-router.register('products', ProductViewSet)
+router.register('hogehoge', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('index/<int:id>/', views.index, name='index'),
+    path('get/<int:id>/', views.index_get, name='index_get'),
+    path('getall/', views.index_get_all, name='index_get_all'),
+    path('post/<str:name>/<int:price>/', views.index_post, name='index_post'),
+    path('patch/<int:id>/<str:name>/<int:price>/', views.index_patch, name='index_patch'),
+    path('delete/<int:id>/', views.index_delete, name='index_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,39 @@
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from django.http import HttpResponse
+import requests
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+def index(request, id):
+    return HttpResponse(f'The ID is: {id}')
+
+
+url = f"http://localhost:8000/products/hogehoge/"
+
+def index_get(request, id):
+    response = requests.get(url + str(id) + "/")
+    text = response.text
+    return HttpResponse(text)
+
+def index_get_all(request):
+    response = requests.get(url)
+    text = response.text
+    return HttpResponse(text)
+
+def index_post(request, name, price):
+    data = {"name": name, "price": price}
+    response = requests.post(url, data=data)
+    return HttpResponse(response.text)
+
+def index_patch(request, id, name, price):
+    data = {"name": name, "price": price}
+    response = requests.patch(url + str(id) + "/", data=data)
+    return HttpResponse(response.text)
+
+def index_delete(request, id):
+    response = requests.delete(url + str(id) + "/")
+    return HttpResponse(response.text)
