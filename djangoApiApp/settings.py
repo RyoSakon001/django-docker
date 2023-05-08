2466d0151e3cb33d92a35e7c5bd979b4b485a919
sakon-ziwave <ryo.development0320@gmail.com>
2023-05-08 11:33:27 +0900

    Swaggerを導入

--- djangoApiApp/settings.py
+++ djangoApiApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'products',
 ]
 
@@ -127,3 +128,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'プロジェクト名',
+    'DESCRIPTION': '詳細',
+    'VERSION': '1.0.0',
+    # api/schemaを表示しない
+    'SERVE_INCLUDE_SCHEMA': False,
+}
